6c2d66797f14a3469afe6a66df922cda104d77da
juan ignacio martinez <jimartinez@fi.uba.ar>
2021-10-05 22:38:20 -0300

    ej7 guia 7

--- guia 7-Tuplas y Listas/ej7.py
+++ guia 7-Tuplas y Listas/ej7.py
@@ -1,3 +1,12 @@
-# Ejercicio 7.7. Escribir una función que reciba una lista de tuplas (Apellido, Nombre, Inicial_
-# segundo_nombre) y devuelva una lista de cadenas donde cada una contenga primero el
+# Ejercicio 7.7. Escribir una función que reciba una lista de tuplas (Apellido, Nombre, Inicial_segundo_nombre) y #devuelva una lista de cadenas donde cada una contenga primero el
 # nombre, luego la inicial con un punto, y luego el apellido.
+
+def main(lista_de_nombres):
+    lista_nueva=[]
+    for i in range(0,len(lista_de_nombres)):
+        datos=list(lista_de_nombres[i])
+        lista_nueva.append(datos[1]+" "+datos[2]+" "+datos[0])
+    return lista_nueva 
+
+print(main([("martinez","pedro","I"),("perez","lucia","M"),("garcia","pablo","J")]))
+
