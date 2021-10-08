8f8378249267e1b148ec0ab8fe10604cfdb29c07
juan ignacio martinez <jimartinez@fi.uba.ar>
2021-10-08 14:38:49 -0300

    e4 guia 9

--- guia 9-Diccionarios/ej4.py
+++ guia 9-Diccionarios/ej4.py
@@ -1,2 +1,19 @@
 # Ejercicio 9.4. Escribir una función que reciba un texto y para cada caracter presente en el texto
 # devuelva la cadena más larga en la que se encuentra ese caracter.
+
+def main(texto):
+    dic = {}
+    texto = texto.split(' ')
+    for palabra in texto:
+        for letra in palabra:
+            if dic.get(letra, ''):
+                if len(palabra) >= len(dic[letra]):
+                    dic[letra] = palabra
+            else: 
+                dic[letra] = palabra
+    return dic
+
+
+texto="anafkhdfhddg kiwi tomate banana zapallo manzana"
+
+print(main(texto))
