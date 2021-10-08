a610914c524755b2443d58ce25df4f097a150c72
juan ignacio martinez <jimartinez@fi.uba.ar>
2021-10-08 12:58:18 -0300

    ej3 guia 9

--- guia 9-Diccionarios/ej3.py
+++ guia 9-Diccionarios/ej3.py
@@ -4,3 +4,27 @@
 # el teléfono y, opcionalmente, permitir modificarlo si no es correcto.
 # b) Si el nombre no se encuentra, debe permitir ingresar el teléfono correspondiente.
 # El usuario puede utilizar la cadena ”*”, para salir del programa.
+
+def main(agenda):
+    while True:
+        nombre=input("ingrese un nombre a buscar en la agenda(ingrese * para no buscar ningun nombre mas): ")
+        if nombre=="*":
+            break
+        if agenda.get(nombre,""):
+            print(agenda[nombre])
+            modificar=input("desea modificar el numero de telefono?(si/no): ")
+            if modificar=="si":
+                nuevo_numero=input("escribir nuevo numero: ")
+                agenda[nombre]=nuevo_numero
+            else:
+                continue
+        else:
+            ingresar=input("el nombre no se encuentra. quiere ingresar un nro para dicha persona?(si/no): ")
+            if ingresar=="si":
+                nuevo_numero=input("ingrese el numero de telefono: ")
+                agenda[nombre]=nuevo_numero
+    return agenda
+                
+agenda={"juan": 43643543, "carlos":3153453, "ana":36757345, "jose": 256436546}
+agenda_modificada=main(agenda)
+print(agenda_modificada)
